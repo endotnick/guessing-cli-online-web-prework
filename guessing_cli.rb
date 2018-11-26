55245394b366f2877f85afc4b765656a7b5e09b8
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 04:47:47 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,10 +6,8 @@ def run_guessing_game
     target = rand(1..6)
     if guess.to_i == target
       puts 'You guessed the correct number!'
-      break
     else
       puts "The computer guessed #{target}"
-      break
     end
   end
   puts "Goodbye!"
